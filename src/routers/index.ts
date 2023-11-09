0406b916fa5930cdb969e2585ce304cbcc022198
Andra <ardhian.hanum@gmail.com>
2023-11-09 13:02:38 +0700

    feat: add login register page

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,22 @@
 import { Request, Response, Router } from "express";
 import { userRouter } from "./user";
 import { authRouter } from "./auth";
+import path from "path";
+
+const rootDirectory = path.resolve('./');
 
 const router = Router();
 router.get('/', (_: Request, res: Response) => {
    res.send('hello world');
-})
+});
+
+router.get('/login', (_: Request, res: Response) => {
+   res.sendFile(rootDirectory + '/src/templates/login.html');
+});
+
+router.get('/register', (_: Request, res: Response) => {
+   res.sendFile(rootDirectory + '/src/templates/register.html');
+});
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
